3c0f1af5be8cda405a160494f1fdcc4838fd6830
Tarasov Ser <111812342+tarasapiens@users.noreply.github.com>
2024-02-07 01:14:16 +0300

    Update Company.java

--- src/main/java/Task4/Company.java
+++ src/main/java/Task4/Company.java
@@ -4,53 +4,59 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+
 public class Company {
 
-    String tittle;
-    int year;
-    List<String> topMovies = new ArrayList<String>();
+    private String tittle;
+    private int year;
+    private List<Movie> topMovies;
 
-    public List<String> getTopMovies() {
-        return topMovies;
-    }
-
-    public void setTopMovies(List<String> topMovies) {
-        this.topMovies = topMovies;
-    }
-
-    public Company(String tittle, int year) {
+    public Company(String tittle, int year, List<Movie> topMovies) {
         this.tittle = tittle;
         this.year = year;
+        this.topMovies = topMovies;
     }
 
     public String getTittle() {
         return tittle;
     }
 
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
     public int getYear() {
         return year;
     }
 
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public List<Movie> getTopMovies() {
+        return topMovies;
+    }
+
+    public void setTopMovies(List<Movie> topMovies) {
+        this.topMovies = topMovies;
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Company company = (Company) o;
-        return year == company.year && Objects.equals(tittle, company.tittle);
+        return year == company.year && Objects.equals(tittle, company.tittle) && Objects.equals(topMovies, company.topMovies);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(tittle, year);
+        return Objects.hash(tittle, year, topMovies);
     }
 
     @Override
     public String toString() {
-        return "Название кинокомпании: " + tittle + '\'' +
-                ", year=" + year +
-                "  Список фильмов " + topMovies +
-                '}';
+        return "Название компании: " + tittle +
+                "  Топ фильмов: " + topMovies;
     }
-
 }
